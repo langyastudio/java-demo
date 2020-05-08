21eff9f5c52bd0ab12cab7c1213e8c6657bac022
shilang ao <jiangjiaxiong@hacfin.com>
2020-05-08 17:53:28 +0800

    no message

--- java/src/StringT.java
+++ java/src/StringT.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class StringT {
     public static void main(String[] args) {
         //--------------------------------------------------------------------------------------------------------------
@@ -35,5 +39,23 @@ public class StringT {
 
         System.out.printf("8x6 的4位结果是：%04d %n", 8 * 6);
         System.out.printf("49.8是：%2.2f %n", 49.8);
+
+        //split join
+        String [] strSplit = "a,b,c,d".split(",");
+        System.out.println(Arrays.toString(strSplit));
+        System.out.println(String.join(":", strSplit));
+
+        //valueof parse
+        System.out.println( String.valueOf(true) );
+        System.out.println( Integer.parseInt("123") );
+        System.out.println( Boolean.parseBoolean("FALSE"));
+        System.out.println( Integer.getInteger("java.version"));
+
+        //code
+        byte[] bArr =  "unicode".getBytes(StandardCharsets.UTF_8);
+        System.out.println(Arrays.toString(bArr));
+        System.out.println(new String(bArr));
+
+
     }
 }
