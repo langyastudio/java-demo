b58e95fecc95098a9199c48e138b1e85f9f1ad81
langyastudio <jiangjiaxiong@hacfin.com>
2020-05-11 06:31:32 +0800

    no message

--- java/src/cls/BeanT.java
+++ java/src/cls/BeanT.java
@@ -1,7 +1,12 @@
 package cls;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
 public class BeanT {
     private int age;
+    private String name;
 
     public int getAge() {
         return age;
@@ -11,8 +16,30 @@ public class BeanT {
         this.age = age;
     }
 
-    public static void main(String[] args) {
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        try
+        {
+            BeanInfo beanInfo = Introspector.getBeanInfo(BeanT.class);
+            for (PropertyDescriptor property : beanInfo.getPropertyDescriptors())
+            {
+                System.out.println(property.getName());
+                System.out.println(property.getReadMethod());
+                System.out.println(property.getWriteMethod());
+                System.out.println(property.getPropertyType());
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
     }
 }
 
---
+++ java/src/cls/EnumT.java
@@ -0,0 +1,35 @@
+package cls;
+
+enum Weekday {
+    SUN(0, "星期天"), MON(1, "星期一"), TUE(2, "星期二"), WED(3, "星期三"), THU(4, "星期四"), FRI(5, "星期五"), SAT(6, "星期六");
+    public final  int    dayValue;
+    private final String chinese;
+
+    private Weekday(int dayValue, String zh) {
+        this.dayValue = dayValue;
+        this.chinese = zh;
+    }
+
+    @Override
+    public String toString() {
+        return this.chinese;
+    }
+}
+
+public class EnumT {
+    public static void main(String[] args) {
+        Weekday day = Weekday.SUN;
+        /*if (day == Weekday.SAT || day == Weekday.SUN) {
+            System.out.println("Work at home!");
+        }
+        else {
+            System.out.println("Work at office!");
+        }*/
+
+        if (day.dayValue == 6 || day.dayValue == 0) {
+            System.out.println("Today is " + day + ". Work at home!");
+        } else {
+            System.out.println("Today is " + day + ". Work at office!");
+        }
+    }
+}
