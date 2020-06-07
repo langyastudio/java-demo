4a97a41f78d6649a7072a6f2e16a5ee83c9a6192
langyastudio <jiangjiaxiong@hacfin.com>
2020-06-07 14:58:45 +0800

    no message

---
+++ java/web/src/main/java/network/SocketClient.java
@@ -0,0 +1,43 @@
+package network;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * socket 编程-客户端
+ */
+public class SocketClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 1234);
+
+        try {
+            handle(socket.getInputStream(), socket.getOutputStream());
+        }
+        finally {
+            socket.close();
+        }
+    }
+
+    private static void handle(InputStream input, OutputStream output) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        var writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+
+        System.out.println("server:" + reader.readLine());
+        Scanner scanner = new Scanner(System.in);
+        for (;;){
+            String s = scanner.nextLine();
+            writer.write(s);
+            writer.newLine();
+            writer.flush();
+
+            String req = reader.readLine();
+            System.out.println(req);
+
+            if(req.equals("bye")){
+                break;
+            }
+        }
+    }
+}
---
+++ java/web/src/main/java/network/SocketServer.java
@@ -0,0 +1,71 @@
+package network;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * socket 编程
+ */
+public class SocketServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(1234);
+        System.out.println("server is running...");
+
+        for (; ; ) {
+            Socket socket = ss.accept();
+            Thread t      = new ServerHandler(socket);
+            t.start();
+        }
+    }
+}
+
+class ServerHandler extends Thread {
+    private Socket ss;
+
+    public ServerHandler(Socket ss) {
+        this.ss = ss;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream  inputStream  = this.ss.getInputStream();
+            OutputStream outputStream = this.ss.getOutputStream();
+
+            handle(inputStream, outputStream);
+        } catch (IOException e) {
+            //关闭socket连接
+            try {
+                this.ss.close();
+            } catch (IOException ioe) {
+            }
+
+            e.printStackTrace();
+            System.out.println("client disconnected.");
+        }
+    }
+
+    private void handle(InputStream input, OutputStream output) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        var writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+
+        writer.write("hello\n"); writer.flush();
+        for (; ; ) {
+            String s = reader.readLine();
+
+            System.out.println(s);
+
+            //退出
+            if (s.equals("bye")) {
+                writer.write("bye\n");
+                writer.flush();
+                break;
+            }
+
+            writer.write("ok: " + s + "\n");
+            writer.flush();
+        }
+    }
+}
