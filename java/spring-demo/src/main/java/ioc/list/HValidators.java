a5c32729f18aba49b9f43e5b23baf2cb0f20456a
jiangjiaxiong <jiangjiaxiong@hacfin.com>
2020-10-13 18:24:24 +0800

    no message

--- java/spring-demo/src/main/java/ioc/list/HValidators.java
+++ java/spring-demo/src/main/java/ioc/list/HValidators.java
@@ -1,15 +1,26 @@
 package ioc.list;
 
+import com.sun.jdi.CharType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class HValidators {
+    @Value("classpath:/app.properties")
+    Resource resource;
+
     @Autowired
     List<HValidator> validators;
 
@@ -17,18 +28,26 @@ public class HValidators {
     ZoneId zoneId = ZoneId.systemDefault();
 
     public void validate(String email, String name) {
-        for(var validator : this.validators){
+        for (var validator : this.validators) {
             validator.validate(email, name);
         }
     }
 
     @PostConstruct
-    public void init(){
+    public void init() {
         System.out.println("init HValidator");
+
+        try (var reader = new BufferedReader(
+                new InputStreamReader(this.resource.getInputStream(), StandardCharsets.UTF_8))) {
+            String logo = reader.lines().collect(Collectors.joining("\n"));
+            System.out.println(logo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @PreDestroy
-    public void shutdown(){
+    public void shutdown() {
         System.out.println("shutdown HValidator");
     }
 }
