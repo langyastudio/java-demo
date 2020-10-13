bf1dbb44d6bc925955e06b0b68857bda8ef5ab12
jiangjiaxiong <jiangjiaxiong@hacfin.com>
2020-10-13 17:11:19 +0800

    add PostConstruct PreDestroy Qualifier test

--- java/spring-demo/src/main/java/AppConfig.java
+++ java/spring-demo/src/main/java/AppConfig.java
@@ -1,15 +1,31 @@
 import ioc.list.HValidators;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 import ioc.UserService;
+
+import java.time.ZoneId;
 
 @Configuration
 //value用于指定要扫描的package
 @ComponentScan(value = "ioc")
 public class AppConfig {
+    /**
+     * 第三方Bean
+     */
+    @Bean
+    @Primary
+    @Qualifier("z") //别名
+    ZoneId createZoneOfZ(){
+        return ZoneId.of("Z");
+    }
+    @Bean
+    @Qualifier("utc8") //别名
+    ZoneId createZoneOfUtc8(){
+        return ZoneId.of("UTC+08:00");
+    }
+
     public static void main(String[] args) {
         //注解装配
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
@@ -18,6 +34,6 @@ public class AppConfig {
 
         //list
         HValidators hValidators = context.getBean(HValidators.class);
-        hValidators.validate("1032030048@qq.com", "");
+        hValidators.validate("1032030048@qq.com", "12");
     }
 }
--- java/spring-demo/src/main/java/ioc/list/HValidators.java
+++ java/spring-demo/src/main/java/ioc/list/HValidators.java
@@ -3,6 +3,9 @@ package ioc.list;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.time.ZoneId;
 import java.util.List;
 
 @Component
@@ -10,9 +13,22 @@ public class HValidators {
     @Autowired
     List<HValidator> validators;
 
+    @Autowired(required = false)
+    ZoneId zoneId = ZoneId.systemDefault();
+
     public void validate(String email, String name) {
         for(var validator : this.validators){
             validator.validate(email, name);
         }
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("init HValidator");
+    }
+
+    @PreDestroy
+    public void shutdown(){
+        System.out.println("shutdown HValidator");
     }
 }
